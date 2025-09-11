42443ba8ec65a49a29f8ea3408d8c0bac4b5793d
Heron Sousa <heronrs1@gmail.com>
2025-09-11 13:18:19 -0300

    chore: configura nativewind

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,22 +12,27 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="scan"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Escanear",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
         }}
       />
     </Tabs>
---
+++ app/(tabs)/scan.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <Text>Escanear</Text>
+  );
+}
